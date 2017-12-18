ef855ccdb64ec1694fa9c5ea5830f36a73a5e322
cameron fuller <cameronmfuller@yahoo.com>
2017-12-17 20:44:12 -0500

    JS done

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,13 +8,12 @@
 */
 
 // initial array of gifs
-var gifs = ["adam sandler", "charlie day", "keanu reeves", "chris pratt"];
+var gifs = ["leonardo dicaprio", "samuel l. jackson", "keanu reeves", "sean connery"];
 
 // function to display gifs
 function displayGifs() {
     //getting the "data-name" attr to plug it into the queryURL
     var gif = $(this).attr("data-name");
-    console.log(this);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=Qt2BqDiQQBWFGVz5QHUoepyDD2e9Jaf0&limit=10&rating=g";
 
     //begin AJAX Call
@@ -26,47 +25,79 @@ function displayGifs() {
         console.log(response);
         var results = response.data;
         $(".gif-area").empty();
+
         // loop through the results and show all the gifs
         for (var i = 0; i < results.length; i++) {
+
             // DISPLAY
+
             // dynamically creating a container div
             var gifContainer = $("<div class='gifContainer'>")
             // appending the rating to the new div
             gifContainer.append("<p>Rating: " + results[i].rating);
-            // creating the img tag
-            var gifImg = $("<img src=" + results[i].images.fixed_height_still.url + ">");
-            // giving attributes useful for pausing andp playing
+            // creating variables to hold the 2 states of the gif
+            var imgStill = results[i].images.fixed_height_still.url;
+            var imgAnimate = results[i].images.fixed_height.url;
+            // add attributes to the gifs
+            var gifImg = $("<img>");
+            gifImg.attr("src", imgStill);
             gifImg.attr("data-state", "still");
-            gifImg.attr("data-still", results[i].images.fixed_height_still.url);
-            gifImg.attr("data-animate", results[i].images.fixed_height.url);
-            // appending the img to the container
+            gifImg.attr("data-still", imgStill);
+            gifImg.attr("data-animate", imgAnimate);
+            gifImg.addClass("gif");
+            // append to the container
             gifContainer.append(gifImg);
-            // appending the new div contents to the page
+            // append to the page
             $(".gif-area").append(gifContainer);
         }
     })
 }
 
+    // pause / play
+
+    function animateGifs() {
+
+        var state = $(this).attr("data-state");
+        console.log(state);
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    };
+
 // function to render buttons
+
 function renderButtons() {
     $("#btn-area").empty();                     // to prevent duplicating buttons
 
     for (var i = 0; i < gifs.length; i++) {     // loops through the array
         var a = $("<button>");                  //create button
-        a.addClass("gif btn btn-default btn-sm btn-primary");   //add class
+        a.addClass("gifBtn btn btn-default btn-sm btn-primary");   //add class
         a.attr("data-name", gifs[i]);           //data attribute
         a.text(gifs[i]);                        //labels the button
         $("#btn-area").append(a);               // append button
     }
 }
+
 // function to add searches to buttons
+
 $(".find-gif").on("click", function (event) {
     event.preventDefault()
     var gif = $(".add-gif").val().trim();       //grab the text
     gifs.push(gif);                             //push to array
-    console.log(gifs);                          //clg
     renderButtons();                            // re-rendering buttons
     $(".add-gif").val("Search for...");         // clearing form after submit
 })
-$(document).on("click", ".gif", displayGifs);
+
+$(document).on("click", ".gifBtn", displayGifs);
+
+// THIS TOOK FOREVER TO FIND
+$(document).on("click", ".gif", animateGifs);
+
 renderButtons();
+
